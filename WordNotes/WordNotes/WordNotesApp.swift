7d310c0378f950d0c91e44afbd710b677b19af07
ppiserchia <piserchia.pasquale@outlook.it>
2024-12-12 00:25:48 +0100

    Initial project setup

---
+++ WordNotes/WordNotes/WordNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordNotesApp.swift
+//  WordNotes
+//
+//  Created by Pasquale Piserchia on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WordNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
